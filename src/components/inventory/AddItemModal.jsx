0a53bf73c86b8a07b1bb617e9669becd41a4cd3b
Andrade, Joseph <josephformentera2@gmail.com>
2025-12-28 19:19:20 +0800

    fix: make inventory modals scrollable

--- src/components/inventory/AddItemModal.jsx
+++ src/components/inventory/AddItemModal.jsx
@@ -91,7 +91,7 @@ const AddItemModal = ({ open, onOpenChange, onAddItem }) => {
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="sm:max-w-[550px]">
+      <DialogContent className="sm:max-w-[550px] max-h-[90vh] overflow-y-auto scrollbar-hide">
         <DialogHeader>
           <DialogTitle className="flex items-center gap-2">
             <Package className="h-5 w-5" />
